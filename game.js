8785de6cd2e509d09e7de7acf00383eaacb126bf
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2017-06-10 13:09:51 +0200

    Split rig into rows

--- game.js
+++ game.js
@@ -10,6 +10,12 @@
     var rig = new Rig(board);
 
     function Rig(board) {
+        this.programs = new Row(board);
+        this.hardware = new Row(board);
+        this.resources = new Row(board);
+    }
+
+    function Row(board) {
         var group = new Konva.Group({
             x: board.width() / 2,
             y: board.height() / 2,
@@ -23,20 +29,34 @@
 
         this.installCard = function install(card) {
             cards.push(card);
-            function sumWidths(totalWidth, card) {
-                return totalWidth + card.sourceWidth;
-            }
+
+            var totalWidth = cards.reduce(
+                function sumWidths(totalWidth, card) {
+                    return totalWidth + card.sourceWidth;
+                },
+                0
+            );
+
+            var maxHeight = cards.reduce(
+                function maximizeHeight(currentMax, card) {
+                    return Math.max(currentMax, card.sourceHeight);
+                },
+                0
+            );
 
             group.add(card.group);
-            var rowScale = group.width() / cards.reduce(sumWidths, 0);
+            var xScale = group.width() / totalWidth;
+            var yScale = group.height() / maxHeight;
+            var scale = Math.min(xScale, yScale);
             var cardsDrawn = 0;
             cards.forEach(
                 function rescaleCards(card) {
                     card.group.scale({
-                        x: rowScale,
-                        y: rowScale
+                        x: scale,
+                        y: scale
                     });
-                    card.group.x(cardsDrawn * card.sourceWidth * rowScale);
+                    card.group.x(cardsDrawn * card.sourceWidth * xScale);
+                    card.draw();
                     cardsDrawn++;
                 }
             );
@@ -45,15 +65,27 @@
     }
 
     function Program(dbId) {
-        this.card = new Card(dbId, 300, 212)
+        var self = this;
+        this.card = new Card(dbId, 300, 212);
+        this.install = function install(rig) {
+            rig.programs.installCard(self.card);
+        }
     }
 
     function Hardware(dbId) {
-        this.card = new Card(dbId, 300, 230)
+        var self = this;
+        this.card = new Card(dbId, 300, 230);
+        this.install = function install(rig) {
+            rig.hardware.installCard(self.card);
+        };
     }
 
     function Resource(dbId) {
-        this.card = new Card(dbId, 300, 195)
+        var self = this;
+        this.card = new Card(dbId, 300, 195);
+        this.install = function install(rig) {
+            rig.resources.installCard(self.card);
+        };
     }
 
     function Card(dbId, sourceWidth, sourceHeight) {
@@ -70,8 +102,7 @@
             draggable: true
         });
 
-        this.install = function install(rig) {
-            rig.installCard(self);
+        this.draw = function draw() {
             var image = new Image();
             image.onload = function drawImage() {
                 var raster = new Konva.Image({
@@ -93,11 +124,11 @@
         }
     }
 
-    new Program("13006").card.install(rig);
-    new Program("10005").card.install(rig);
-    new Program("01028").card.install(rig);
-    new Hardware("01024").card.install(rig);
-    new Hardware("02085").card.install(rig);
-    new Resource("05048").card.install(rig);
+    new Program("13006").install(rig);
+    new Program("10005").install(rig);
+    new Program("01028").install(rig);
+    new Hardware("01024").install(rig);
+    new Hardware("02085").install(rig);
+    new Resource("05048").install(rig);
 
 })(window);
